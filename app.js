639aa85959ec9ca1124efaca7edf7e8356cd9b25
unknown <rachel.luo887@gmail.com>
2026-06-22 14:54:02 -0400

    feat: intial javascript file

---
+++ app.js
@@ -0,0 +1,7 @@
+
+// Starter code from Lab3
+<script src="js/script.js" defer></script>
+function updateWelcomeMessage() {
+    const message = document.querySelector("#welcome-message");
+    message.textContent = "Welcome to our interactive CSI 3140 website!";
+}
