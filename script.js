1a790f7655170ee13d7d822cb1e694a904f2710b
rivpii <oli.xiwen.zhao@gmail.com>
2026-06-26 00:18:27 -0400

    Got most of the form validation done. Am debating removing and replacing the password input field with something else. Also brushed up the form submission validation. Need to work on improving the easter egg.

--- script.js
+++ script.js
@@ -53,6 +53,27 @@ if (announcementsList) {
 
 //Provide: 1. error messages when invalid, 2. Prevent submission when invalid, 3. success messages when successful
 
+let failedAttempts = 0;
+
+const failureMessages = [
+    "Wow, you're pretty bad at this.",
+    "Maybe you should get an adult or friend to help you.",
+    "How hard is it to read???",
+    "It's a wonder you managed to even navigate here.",
+    "Aren't you a University Student? How is it this hard to fill a form correctly?",
+    "...You do know how to read right?",
+    "I refuse to believe you have a functional frontal lobe.",
+    "Don't just spam the button!",
+    "You are not qualified to be a member of this club",
+    "I'm regretting my life decisions.",
+    "I'm not paid enough for this",
+    "This is just...sad.",
+    "You're doing this for fun now, huh?",
+    "You think this is funny? I have a wife and children!",
+    "The Cheese is disappointed in you."
+];
+
+
 //NOTE: This is the default code given
 console.log("Before form lookup");
 const form = document.querySelector("#registration-form");
@@ -87,14 +108,9 @@ if(form){ //makes sure this only runs when form exists (ie. on registration.html
      const password = document.querySelector("#password");
      const passwordError = document.querySelector("#password-error");
 
-     //Dropdown
-
-     //Radio
-
-     //Checkbox
-
      //Success message
      const success = document.querySelector("#success-message");
+     const failure = document.querySelector("#failure-message");
 
      //TO TRACK IF ERRORS EXIST
      let valid = true;
@@ -125,6 +141,8 @@ if(form){ //makes sure this only runs when form exists (ie. on registration.html
          lastname.classList.remove("input-error");
      }
 
+     //Email
+
      if (email.value.trim() === ""){
          emailError.textContent = "Please enter your email address.";
 
@@ -132,10 +150,19 @@ if(form){ //makes sure this only runs when form exists (ie. on registration.html
 
          valid = false;
      }
+     else if (!email.value.includes("@")){ //Email Validation check
+        emailError.textContent = "Please enter a valid email address.";
+         email.classList.add("input-error");
+         valid = false;
+     }
      else{
          emailError.textContent = "";
          email.classList.remove("input-error");
      }
+
+
+     //SSN
+     const ssnPattern = /^\d{9}$/; //Trying to define the parameters for the SSN (9 digits long, 0-9)
 
      if(SSN.value.trim() === ""){
          SSNError.textContent = "Please enter you SSN.";
@@ -143,14 +170,36 @@ if(form){ //makes sure this only runs when form exists (ie. on registration.html
 
         valid = false;
     }
+    else if(!ssnPattern.test(SSN.value)){ //SSN format check
+        //Check if it's running.
+        console.log("SSN value:", SSN.value);
+        console.log("Regex result:", ssnPattern.test(SSN.value));
+
+        SSNError.textContent = "SSN must contain exactly 9 digits.";
+
+        SSN.classList.add("input-error");
+        
+        valid = false;
+
+    }
     else{
         SSNError.textContent = "";
         SSN.classList.remove("input-error");
 
     }
 
-    if (phone.value.trim() === ""){
+    //Phone number
+    const phonePattern = /^\d{10}$/;
+    const phoneValue = phone.value.trim();
+
+    if (phoneValue === ""){
         phoneError.textContent = "Please enter your phone number.";
+        phone.classList.add("input-error");
+
+        valid = false;
+    }
+    else if(!phonePattern.test(phoneValue)){ //Phone number format check
+        phoneError.textContent = "Please enter a valid phone number.";
         phone.classList.add("input-error");
 
         valid = false;
@@ -160,6 +209,7 @@ if(form){ //makes sure this only runs when form exists (ie. on registration.html
         phone.classList.remove("input-error");
     }
 
+    //TODO: MAY REMOVE PASSWORD FROM FORM ENTIRELY
     if(password.value.trim() ===""){
         passwordError.textContent = "Please enter your password.";
         password.classList.add("input-error");
@@ -171,31 +221,93 @@ if(form){ //makes sure this only runs when form exists (ie. on registration.html
         password.classList.remove("input-error");
     }
 
-     //Email validation
-     if (!email.value.includes("@")) {
-         emailError.textContent = "Please enter a valid email address.";
-         email.classList.add("input-error");
-         valid = false;
-     } else {
-         emailError.textContent = "";
-         email.classList.remove("input-error");
-     }
+    //Textarea
+    const favoriteCheese = document.querySelector("#favorite_cheese");
+    const favoriteError = document.querySelector("#favorite-error");
+    if(favoriteCheese.value.trim() === ""){
+        favoriteError.textContent = "State your favorite Cheese(s).";
+        favoriteCheese.classList.add("input-error");
 
-    //Number formatting validation. Includes: SSN, phone, possibly password
-    //TODO: MIGHT REMOVE PASSWORD FIELD ENTIRELY and replace it!
+        valid = false;
+    }
+    else{
+        favoriteError.textContent = "";
+        favoriteCheese.classList.remove("input-error");
+    }
+
+    //Radio buttons
+    const knowledge = document.querySelector('input[name="cheese_knowledge"]:checked');
+    const knowledgeError = document.querySelector("#knowledge-error");
+
+    if (!knowledge){
+        knowledgeError.textContent = "Please select your level of cheese expertise.";
+
+        valid = false;
+    }
+    else{
+        knowledgeError.textContent = "";
+    }
+
+    //Dropdown
+    const gender = document.querySelector("#gender");
+    const genderError = document.querySelector("#gender-error");
+    if(gender.value === ""){
+        genderError.textContent = "Choose your gender.";
+        gender.classList.add("input-error");
+        
+        valid = false;
+    }
+    else{
+        genderError.textContent = "";
+        gender.classList.remove("input-error");
+    }
+
+    //Checkbox //TODO: FIX!!!!!
+    const terms = document.querySelector("#terms");
+    const termsError = document.querySelector("#terms-error");
+
+    if (!terms.checked){
+        termsError.textContent = "Pledge yourself to the Cheese Connoisseur Club.";
+        terms.classList.add("input-error");
+
+        valid =false;
+
+    }
+    else{
+        termsError.textContent = "";
+        terms.classList.remove("input-error");
+    }
 
 
-     //Is this correct? REVIEW THIS
+     //Check Submission Validity
      if(!valid){
+
+         failedAttempts++;
+         console.log(failedAttempts);
+
          console.log("Invalid Submission"); //for documentation purposes?
          success.style.display = "none";
+         failure.style.display = "block";
+
+         if(failedAttempts % 5 === 0){
+            const i = Math.floor(failedAttempts / 5 - 1) % failureMessages.length;
+
+            failure.textContent = failureMessages[i];
+         }
+         else{
+            failure.textContent = "Registration Failed. Please fix the highlighted fields.";
+         }
+        
      }
      else{
 
          form.reset(); //reset to empty the form after good submission
         
+         failure.style.display = "none";  
          success.textContent = "Registration successful!";
-         success.style.display = "block";     
+         success.style.display = "block";
+
+         failedAttempts = 0;
 
          //wipe the error messages too
          document.querySelectorAll(".error-message").forEach(error => {
